b5433378d4bcb4688248c6282283197da86c0153
Sumit <sumokhan76342@gmail.com>
2024-06-23 15:11:56 +0530

    Activating Like video button

--- src/routes/video.js
+++ src/routes/video.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { uploadVideo, getAllVideos } from '../controllers/video.js'
+import { likeController } from '../controllers/like.js'
 import upload from '../Helpers/fileHelpers.js'
 
 
@@ -7,5 +8,5 @@ const routes = express.Router();
 
 routes.post('/uploadVideo', upload.single("file"), uploadVideo)
 routes.get('/getvideos', getAllVideos)
-
+routes.patch('/like/:id', likeController)
 export default routes;
